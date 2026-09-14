d6d0b0147876df5f67d25c22107a40ef37b5b167
santiago <a01255853@tec.mx>
2026-09-14 15:44:13 -0600

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list) # mapping char count of each string to list of anagrams
+        
+        for s in strs:
+            count = [0] * 26 # a - z
+            
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            result[tuple(count)].append(s)
+
+        return list(result.values())
+        
